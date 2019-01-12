81f3dbba7b74753696a26ec570da841bec0a0970
Chris Gearing <gearichr@turntown.com>
2019-01-12 17:21:57 +0000

    linting fixes

--- lexicorm/conversion.py
+++ lexicorm/conversion.py
@@ -5,9 +5,17 @@ import inflection
 from .type import JSONType
 
 
-def python_dict_to_json_dict(input: Mapping[str, Any]) -> Mapping[str, JSONType]:
-    return {inflection.camelize(key, False): value for key, value in input.items()}
+def python_dict_to_json_dict(
+        input: Mapping[str, Any]
+) -> Mapping[str, JSONType]:
+    return {
+        inflection.camelize(key, False): value for key, value in input.items()
+    }
 
 
-def json_dict_to_python_dict(input: Mapping[str, JSONType]) -> Mapping[str, JSONType]:
-    return {inflection.underscore(key): value for key, value in input.items()}
+def json_dict_to_python_dict(
+        input: Mapping[str, JSONType]
+) -> Mapping[str, JSONType]:
+    return {
+        inflection.underscore(key): value for key, value in input.items()
+    }
