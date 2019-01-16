b29dc01e6535cd5bf3d6a3b7b6d82e62b2d79243
Chris Gearing <gearichr@turntown.com>
2019-01-16 11:38:26 +0000

    exclude test directories from distribution

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='lexicorm',
-    version='0.0.2',
+    version='0.0.4',
     author='Chris Gearing',
     author_email="chrisagearing@outlook.com",
     description="An ORM helper package",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/cgearing/lexicorm',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['test', 'test.*']),
+    python_requires=">=3.6",
+    install_requires=[
+        'sqlalchemy>=1.2'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
