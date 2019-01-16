044747174a8373790bc44d2d6ac8e0b4c02ca137
Chris Gearing <gearichr@turntown.com>
2019-01-16 11:12:08 +0000

    update setup for new build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='lexicorm',
-    version='0.0.1',
+    version='0.0.2',
     author='Chris Gearing',
     author_email="chrisagearing@outlook.com",
     description="An ORM helper package",
